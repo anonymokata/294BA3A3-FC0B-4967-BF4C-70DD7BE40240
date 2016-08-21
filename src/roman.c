b611732a8a39d3cd38a7f926563793a54ae8f68d
anonymous <anonymous>
2016-08-21 18:20:11 +0000

    refactor: quick clarifying comments and chunking

--- src/roman.c
+++ src/roman.c
@@ -12,10 +12,12 @@ const char* roman_add(const char* num1, const char* num2)
     if ((num1 == 0) || (num2 == 0))
         return 0;
     
+    //grab lengths and allocate enough memory for us to build the final string
     len1 = strlen(num1);
     len2 = strlen(num2);
-    
     retval = malloc(len1 + len2 + 1);
+    
+    //concat the two strings to get added string
     sprintf(retval, "%s%s", num1, num2);
     
     return retval;
