305d64f6a0dcdcf3caee442efeef4137e64c793e
anonymous <anonymous>
2016-08-21 18:07:15 +0000

    green: add I + I

--- src/roman.c
+++ src/roman.c
@@ -3,5 +3,14 @@
 
 const char* roman_add(const char* num1, const char* num2)
 {
-    return NULL;
+    char* retval;
+    size_t len1, len2;
+    
+    len1 = strlen(num1);
+    len2 = strlen(num2);
+    
+    retval = malloc(len1 + len2 + 1);
+    sprintf(retval, "%s%s", num1, num2);
+    
+    return retval;
 }
