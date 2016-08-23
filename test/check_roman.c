e3d0c239fa6784edad9d8c831dfa5188ffc34fee
anonymous <anonymous>
2016-08-23 11:21:35 +0000

    red: add with M and to M

--- test/check_roman.c
+++ test/check_roman.c
@@ -342,6 +342,27 @@ START_TEST(add_with_D_and_to_D)
 }
 END_TEST
 
+START_TEST(add_with_M_and_to_M)
+{
+    const char* result;
+    
+    result = roman_add("D","D");
+    ck_assert_ptr_ne(0, result);
+    ck_assert_str_eq("M", result);
+    free((void*)result);
+    
+    result = roman_add("MD","D");
+    ck_assert_ptr_ne(0, result);
+    ck_assert_str_eq("MM", result);
+    free((void*)result);
+    
+    result = roman_add("C","CM");
+    ck_assert_ptr_ne(0, result);
+    ck_assert_str_eq("M", result);
+    free((void*)result);
+}
+END_TEST
+
 
 /* CASE: handle errors */
 START_TEST(null_inputs_should_return_null)
@@ -400,6 +421,7 @@ Suite* roman_suite(void)
     tcase_add_test(c, add_to_C);
     tcase_add_test(c, add_compacted_numbers);
     tcase_add_test(c, add_with_D_and_to_D);
+    tcase_add_test(c, add_with_M_and_to_M);
     suite_add_tcase(s, c);
     
     c = tcase_create("errors");
